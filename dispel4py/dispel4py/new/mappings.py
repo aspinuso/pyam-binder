adff700514bdc1a2b1bb78912bb0ca1056ad39e5
Alessandro Spinuso <spinuso@knmi.nl>
2019-10-28 14:36:23 +0100

    dispel4py

--- dispel4py/dispel4py/new/mappings.py
+++
@@ -1,7 +0,0 @@
-config = {
-    "mpi": "dispel4py.new.mpi_process",
-    "multi": "dispel4py.new.multi_process",
-    "simple": "dispel4py.new.simple_process",
-    "storm": "dispel4py.storm.storm_submission",
-    "spark": "dispel4py.new.spark_process"
-}
